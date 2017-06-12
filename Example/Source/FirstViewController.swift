e5648113364c03e531deab33a23da1eef75c41d7
Ollie Wagner <olliewagner@mac.com>
2017-06-12 15:17:09 -0600

    Stub for example project

---
+++ Example/Source/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  YapAnimatorExample
+//
+//  Created by Ollie Wagner on 6/12/17.
+//  Copyright © 2017 Yap Studios. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view, typically from a nib.
+	}
+
+	override func didReceiveMemoryWarning() {
+		super.didReceiveMemoryWarning()
+		// Dispose of any resources that can be recreated.
+	}
+
+
+}
+
---
+++ Podfile
@@ -0,0 +1,11 @@
+# Define a global platform for our project
+platform :ios, '9.0'
+
+source 'git@github.com:CocoaPods/Specs.git'
+use_frameworks!	# needed to support Swift-based pods
+
+target 'YapAnimatorExample' do
+
+# normal pods (read-only)
+	pod 'YapAnimator', :path => './'
+end
