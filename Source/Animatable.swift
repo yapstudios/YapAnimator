7c3dc9dc2291750d5c87ef4d5663122cc62a4519
Ollie Wagner <olliewagner@mac.com>
2017-06-14 11:10:19 -0600

    Updated format of authorship attribution at top of each file

--- Source/Animatable.swift
+++ Source/Animatable.swift
@@ -20,7 +20,11 @@ CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,
 TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
 SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 
-Authored by Ollie Wagner (ollie@yapstudios.com). Please feel free to send an email to me if you have any questions.
+---
+
+Authored by Ollie Wagner (ollie@yapstudios.com). Please feel free to
+send an email to me if you have any questions.
+
 */
 
 // Note: When adding `Animatable` conformance to types, be careful that the constructor does not clip or otherwise restrict values.
